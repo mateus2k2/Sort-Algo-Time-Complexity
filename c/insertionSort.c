92ef5898715dcd3866b6dee79bc15ac0309743c4
Mateus <mateusfilipe2k2@gmail.com>
2023-05-23 15:34:06 -0300

    init

---
+++ c/insertionSort.c
@@ -0,0 +1,16 @@
+// https://www.programiz.com/dsa/insertion-sort
+
+#include <stdio.h>
+
+void insertionSort(int array[], int size){
+    for (int step = 1; step < size; step++){
+        int key = array[step];
+        int j = step - 1;
+
+        while (key < array[j] && j >= 0){
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
