4e89f941b0829ba8f6ed304e6c9c14475258ce01
Mateus <mateusfilipe2k2@gmail.com>
2023-05-24 21:40:25 -0300

    make

---
+++ makefile
@@ -0,0 +1,43 @@
+#make -s --> Suppresses other makefile messages
+
+all: driver.o insertionSort.o mergeSort.o radixSort.o 
+	@g++ ./src/obj/driver.o ./src/obj/insertionSort.o ./src/obj/mergeSort.o ./src/obj/radixSort.o -o ./bin/runner.exe -Wall
+
+degub: 
+	@g++ ./src/*.c -o ./bin/degub.exe -g
+
+degubRun:
+	@gdb ./bin/degub.exe
+
+# ------------------------------------------------------------------------------
+
+driver.o: ./src/driver.c
+	@g++ -c ./src/driver.c -o ./src/obj/driver.o -Wall
+
+insertionSort.o: ./src/insertionSort.c
+	@g++ -c ./src/insertionSort.c -o ./src/obj/insertionSort.o -Wall
+
+mergeSort.o: ./src/mergeSort.c
+	@g++ -c ./src/mergeSort.c -o ./src/obj/mergeSort.o -Wall
+
+radixSort.o: ./src/radixSort.c
+	@g++ -c ./src/radixSort.c -o ./src/obj/radixSort.o -Wall
+
+# ------------------------------------------------------------------------------
+
+run:
+	@./bin/runner.exe > ./out/output.txt
+
+# ------------------------------------------------------------------------------
+
+go:
+	@make -s clean
+	@make -s all
+	@make -s run
+
+# ------------------------------------------------------------------------------
+
+clean:
+	# @cd ./bin && rm ./*.exe 
+	# @cd ./src/obj && rm ./*.o
+	# @clear
---
+++ src/driver.c
@@ -0,0 +1,87 @@
+#include "headers/insertionSort.h"
+#include "headers/mergeSort.h"
+#include "headers/radixSort.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void printArray(int array[], const char string[], int size, double tempoGasto) {
+    printf("\n%s\n", string);
+    printf("%.20f\n", tempoGasto);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+}
+
+int* generateInstancia(int n) {
+    int* array = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        array[i] = rand() % n;
+    }
+
+    return array;
+}
+
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
+    clock_t start, end;
+    double tempoGastoInsertion;
+    double tempoGastoMerge;
+    double tempoGastoRadix;
+
+    int size = 0;
+
+    for (int i = 100; i < 400; i = i * 2) {
+        printf("\n\n------------------------------------------------------------------------\n\n");
+        printf("Tamanho Instancia %i\n", i);
+        for (int j = 0; j < 20; j++) {
+            printf("\n\n\n***Instancia %i/20***\n", j + 1);
+            int* instancia = generateInstancia(i);
+            size = i;
+
+            int* instanciaInsertionSort = (int*)malloc(size * sizeof(int));
+            memcpy(instanciaInsertionSort, instancia, size * sizeof(int));
+
+            int* instanciaMergeSort = (int*)malloc(size * sizeof(int));
+            memcpy(instanciaMergeSort, instancia, size * sizeof(int));
+
+            int* instanciaRadixSort = (int*)malloc(size * sizeof(int));
+            memcpy(instanciaRadixSort, instancia, size * sizeof(int));
+
+            printArray(instancia, "Instancia Real", size, 0);
+            
+            start = clock();
+            insertionSort(instanciaInsertionSort, size);
+            end = clock();
+            tempoGastoInsertion = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+            printArray(instanciaInsertionSort, "Sorted With Insertion Sort", size, tempoGastoInsertion);
+            
+            start = clock();
+            mergeSort(instanciaMergeSort, 0, size - 1);
+            end = clock();
+            tempoGastoMerge = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+            printArray(instanciaMergeSort, "Sorted With Merge Sort", size, tempoGastoMerge);
+            
+            start = clock();
+            radixsort(instanciaRadixSort, size);
+            end = clock();
+            tempoGastoRadix = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+            printArray(instanciaRadixSort, "Sorted With Radix Sort", size, tempoGastoRadix);
+
+            free(instanciaInsertionSort);
+            free(instanciaMergeSort);
+            free(instanciaRadixSort);
+            free(instancia);
+        }
+    }
+
+    return 0;
+}
