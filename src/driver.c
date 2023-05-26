6d4497f6c1d7d71fc71d322a9fd7992eb9b610a5
Mateus <mateusfilipe2k2@gmail.com>
2023-05-25 21:50:49 -0300

    csv

--- src/driver.c
+++ src/driver.c
@@ -7,21 +7,29 @@
 #include <time.h>
 #include <string.h>
 
-void printArray(int array[], const char string[], int size, double tempoGasto) {
-    printf("\n%s\n", string);
-    printf("%.20f\n", tempoGasto);
-
+void printArray(int array[], int size){
     for (int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
 }
 
-void generateInstancia(int *array, int size) {
+void printValues(int size, int instanceIndex, double insertionTime, double mergeTime, double radixTime, int arrayReal[], int arrayInsertion[], int arrayMerge[] ,int arrayRadix[]) {
+    printf("%d, %d, %.20f, %.20f, %.20f\n", size, instanceIndex, insertionTime, mergeTime, radixTime);
+    // printf(", "); 
+    // printArray(arrayReal, size);
+    // printf(", "); 
+    // printArray(arrayInsertion, size);
+    // printf(", "); 
+    // printArray(arrayMerge, size);
+    // printf(", "); 
+    // printArray(arrayRadix, size);
+    // printf("\n");
+}
 
+void generateInstancia(int *array, int size) {
     for (int i = 0; i < size; i++) {
         array[i] = rand() % size;
     }
-
 }
 
 int main(int argc, char* argv[]) {
@@ -34,11 +42,11 @@ int main(int argc, char* argv[]) {
 
     int size = 0;
 
-    for (int i = 100; i < 10000; i = i * 2) {
-        printf("\n\n------------------------------------------------------------------------\n\n");
-        printf("Tamanho Instancia %i\n", i);
+    printf("Size, Instance Index, Insertion Time, Merge Time, Radix Time\n");
+    // printf("Size, Instance Index, Insertion Time, Merge Time, Radix Time, Real Array, Insertion Array, Merge Array, Radix Array\n");
+
+    for (int i = 100; i < 1000; i = i * 2) {
         for (int j = 0; j < 20; j++) {
-            printf("\n\n\n***Instancia %i/20***\n", j + 1);
             size = i;
             int* instancia = (int*)malloc(size * sizeof(int));
             generateInstancia(instancia, size);
@@ -52,34 +60,30 @@ int main(int argc, char* argv[]) {
             int* instanciaRadixSort = (int*)malloc(size * sizeof(int));
             memcpy(instanciaRadixSort, instancia, size * sizeof(int));
 
-            printArray(instancia, "Instancia Real", size, 0);
-            
             start = clock();
             insertionSort(instanciaInsertionSort, size);
             end = clock();
             tempoGastoInsertion = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-            printArray(instanciaInsertionSort, "Sorted With Insertion Sort", size, tempoGastoInsertion);
-            
             start = clock();
             mergeSort(instanciaMergeSort, 0, size - 1);
             end = clock();
             tempoGastoMerge = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-            printArray(instanciaMergeSort, "Sorted With Merge Sort", size, tempoGastoMerge);
-            
             start = clock();
             radixsort(instanciaRadixSort, size);
             end = clock();
             tempoGastoRadix = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-            printArray(instanciaRadixSort, "Sorted With Radix Sort", size, tempoGastoRadix);
+            printValues(size, j + 1, tempoGastoInsertion, tempoGastoMerge, tempoGastoRadix, instancia, instanciaInsertionSort, instanciaMergeSort, instanciaRadixSort);
 
             free(instanciaInsertionSort);
             free(instanciaMergeSort);
             free(instanciaRadixSort);
             free(instancia);
         }
+
+        printf("\n");
     }
 
     return 0;
